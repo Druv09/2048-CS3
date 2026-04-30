02863d74706174e82aaaf3c873e36408d407e007
Hemish Duri <durihemish@gmail.com>
2026-04-30 12:05:14 -0500

    save

--- main.js
+++ main.js
@@ -1,5 +1,38 @@
-const readline = require('readline')
-const matrix = [];
-matrix.push([], [], [], [])
+import readline from 'node:readline';
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
 
-const g = std.readline();
+const matrix = [];
+const dict = [];
+let finalNumber = 0;
+let gameEnd;
+
+
+rl.question(`What difficulty? hard/med/easy`, type => {
+    difficulty = type;
+});
+
+if(difficulty = "easy")
+{
+    matrix.push([], [], [], [], [], [], [], []);
+    dict.push(2, 2, 2, 2, 4, 4, 4, 4, 4, 4);
+}
+
+if(difficulty = "med")
+{
+    matrix.push([], [], [], []);
+    dict.push(2, 2, 2, 2, 2, 2, 2, 2, 2, 4);
+}
+
+if(difficulty = "hard")
+{
+    matrix.push([], [], [], []);
+    dict.push(2, 2, 2, 2, 2, 2, 2, 2, 2, 2);
+}
+
+while()
+{
+    
+}
